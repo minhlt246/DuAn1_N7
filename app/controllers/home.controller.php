dcd173cb3967391c9fcc394fabb5e929d3b3846c
chuong-lee <chuongldps37998@gmail.com>
2024-11-05 22:57:22 +0700

    Header, footer

---
+++ app/controllers/home.controller.php
@@ -0,0 +1,11 @@
+<!-- b1: tạo the php -->
+<!-- b2: tạo class có tên là home controller -->
+<!-- b3: tạo contrutor chứa tham số  -->
+<!-- b4: tạo hàm chức năng -->
+ <?php
+ class HomeController{
+    public function renderHome(){
+        require_once './views/content/trangchu.php';
+    }
+ }
+ ?>
