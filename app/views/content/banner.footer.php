e8752d397e0744512c7eb8e3232293e9f5219876
chuong-lee <chuongldps37998@gmail.com>
2024-11-06 23:15:17 +0700

    thiet ke trang chu

---
+++ app/views/content/banner.footer.php
@@ -0,0 +1,7 @@
+<div id="carouselExampleSlidesOnly" class="carousel slide banner-slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="../public/asset/image/banner.png" class="d-block w-100" alt="..." style="max-height: 300px;">
+    </div>
+  </div>
+</div>
