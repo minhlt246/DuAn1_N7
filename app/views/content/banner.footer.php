2c36b45ea4cdd53737104b55ab12e1b4c036e01a
chuong-lee <chuongldps37998@gmail.com>
2024-11-14 22:25:36 +0700

    fix ui trang chu

--- app/views/content/banner.footer.php
+++ app/views/content/banner.footer.php
@@ -1,7 +1,107 @@
-<div id="carouselExampleSlidesOnly" class="carousel slide banner-slide" data-bs-ride="carousel">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src="../public/asset/image/banner.png" class="d-block w-100" alt="..." style="max-height: 300px;">
-    </div>
-  </div>
-</div>
+<div class="row">
+            <div class="col-md-8 col-lg-6 mx-auto wow fadeInLeft" data-wow-delay="0.3s" data-wow-duration="0.5s">
+              <div class="new-img-block"><img class="w-100" src="../public/client/images/product-love-1.png"></div>
+            </div>
+            <div class="col-lg-6">
+              <div class="road-content">
+                <div class="responsive">
+                  <div class="col-lg-4">
+                    <div class="px-0 wow flipInX" data-wow-delay="0.5s" data-wow-duration="0.9s">
+                      <div class="we-did-block"><img class="w-100 block-img" src="../public/client/images/love-product-0.jpeg" alt="...">
+                        <div class="we-did-content">
+                          <div class="w-100 text-center">
+                            <div class="h6 text-center mb-3">Traditional advertisins<br>does not work well anymore.</div>
+                            <div class="text-center"><small>December 29, 2022</small></div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-lg-4">
+                    <div class="px-0 wow flipInX" data-wow-delay="0.5s" data-wow-duration="0.9s">
+                      <div class="we-did-block"><img class="w-100 block-img" src="../public/client/images/love-product-1.jpeg" alt="...">
+                        <div class="we-did-content">
+                          <div class="w-100 text-center">
+                            <div class="h6 text-center mb-3">Traditional advertisins<br>does not work well anymore.</div>
+                            <div class="text-center"><small>December 29, 2022</small></div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-lg-4">
+                    <div class="px-0 wow flipInX" data-wow-delay="0.5s" data-wow-duration="0.9s">
+                      <div class="we-did-block"><img class="w-100 block-img" src="../public/client/images/love-product-2.jpeg" alt="...">
+                        <div class="we-did-content">
+                          <div class="w-100 text-center">
+                            <div class="h6 text-center mb-3">Traditional advertisins<br>does not work well anymore.</div>
+                            <div class="text-center"><small>December 29, 2022</small></div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-lg-4">
+                    <div class="px-0 wow flipInX" data-wow-delay="0.5s" data-wow-duration="0.9s">
+                      <div class="we-did-block"><img class="w-100 block-img" src="../public/client/images/love-product-3.jpeg" alt="...">
+                        <div class="we-did-content">
+                          <div class="w-100 text-center">
+                            <div class="h6 text-center mb-3">Traditional advertisins<br>does not work well anymore.</div>
+                            <div class="text-center"><small>December 29, 2022</small></div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-lg-4">
+                    <div class="px-0 wow flipInX" data-wow-delay="0.5s" data-wow-duration="0.9s">
+                      <div class="we-did-block"><img class="w-100 block-img" src="../public/client/images/love-product-4.jpeg" alt="...">
+                        <div class="we-did-content">
+                          <div class="w-100 text-center">
+                            <div class="h6 text-center mb-3">Traditional advertisins<br>does not work well anymore.</div>
+                            <div class="text-center"><small>December 29, 2022</small></div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-lg-4">
+                    <div class="px-0 wow flipInX" data-wow-delay="0.5s" data-wow-duration="0.9s">
+                      <div class="we-did-block"><img class="w-100 block-img" src="../public/client/images/love-product-5.jpeg" alt="...">
+                        <div class="we-did-content">
+                          <div class="w-100 text-center">
+                            <div class="h6 text-center mb-3">Traditional advertisins<br>does not work well anymore.</div>
+                            <div class="text-center"><small>December 29, 2022</small></div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-lg-4">
+                    <div class="px-0 wow flipInX" data-wow-delay="0.5s" data-wow-duration="0.9s">
+                      <div class="we-did-block"><img class="w-100 block-img" src="../public/client/images/love-product-6.jpeg" alt="...">
+                        <div class="we-did-content">
+                          <div class="w-100 text-center">
+                            <div class="h6 text-center mb-3">Traditional advertisins<br>does not work well anymore.</div>
+                            <div class="text-center"><small>December 29, 2022</small></div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-lg-4">
+                    <div class="px-0 wow flipInX" data-wow-delay="0.5s" data-wow-duration="0.9s">
+                      <div class="we-did-block"><img class="w-100 block-img" src="../public/client/images/love-product-7.jpeg" alt="...">
+                        <div class="we-did-content">
+                          <div class="w-100 text-center">
+                            <div class="h6 text-center mb-3">Traditional advertisins<br>does not work well anymore.</div>
+                            <div class="text-center"><small>December 29, 2022</small></div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
--- app/views/header.php
+++ app/views/header.php
@@ -1,56 +1,73 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta http-equiv="X-UA-Compatible" content="IE=Edge">
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="keywords" content="">
-    <meta name="description" content="">
-    <title>Ubrand-shop</title>
-    <link rel="icon" href="../public/client/images/logo.png" type="image/gif" sizes="16x16">
-    <!-- main css-->
-    <link rel="stylesheet" href="../public/client/css/bootstrap.min.css">
-    <link rel="stylesheet" href="../public/client/css/all.min.css">
-    <link rel="stylesheet" href="../public/client/css/hover-min.css">
-    <link rel="stylesheet" href="../public/client/css/flag-icons.min.css">
-    <link rel="stylesheet" href="../public/client/css/style.css">
-    <link rel="stylesheet" href="../public/client/slick/slick.css">
-    <!--link(rel='stylesheet' href='client/fonts/fontstyle.css')-->
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Muli:300,400,600,700&amp;subset=latin,latin-ext" type="text/css" media="all">
-  </head>
-  <body>
-    <header>
-      <nav class="navbar navbar-expand-lg menu">
-        <div class="container"><a class="navbar-brand" href="#"><img class="img-logo" src="../public/client/images/logo.png"></a>
-          <button class="navbar-toggler border-0" type="button" data-bs-toggle="offcanvas" data-bs-target="#menu" aria-controls="offcanvasNavbar" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
-          <div class="search-home-page">
-                <button type="button" class="">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
-                        <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
-                    </svg>
-                </button>
-                <input type="text" placeholder="Tìm kiếm sản phẩm">
-            </div>
-          <div class="offcanvas offcanvas-start" id="menu" tabindex="-1" aria-labelledby="offcanvasNavbarLabel">
-            <div class="offcanvas-header">
-              <div class="offcanvas-title"><img class="img-logo" src="../public/client/images/logo.png"></div>
-              <button class="btn-close" type="button" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-            </div>
-            
-            <div class="offcanvas-body">
-              <ul class="navbar-nav justify-content-center flex-grow-1 pe-3 text-uppercase">
-                <li class="nav-item mx-lg-4"><a class="nav-link" href="index.html">Trang Chủ</a></li>
-                <li class="nav-item mx-lg-4"><a class="nav-link" href="product.html">Sản Phẩm</a></li>
-                <li class="nav-item mx-lg-4"><a class="nav-link" href="system.html">Tin tức</a></li>
-                <li class="nav-item mx-lg-4"><a class="nav-link" href="overview.html">Giới thiệu</a></li>
-                <li class="nav-item mx-lg-4"><a class="nav-link" href="contact.html">Liên Hệ</a></li>
-              </ul>
-              <div class="d-grid gap-2 d-md-flex justify-content-md-end">
-                <button class="btn btn-primary me-md-2" type="button">Đăng KÍ</button>
-                <button class="btn btn-primary" type="button">ĐĂng Nhập</button>
-              </div>
+
+<head>
+  <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="keywords" content="">
+  <meta name="description" content="">
+  <title>Ubrand-shop</title>
+  <link rel="icon" href="../public/client/images/logo.png" type="image/gif" sizes="16x16">
+  <!-- main css-->
+  <link rel="stylesheet" href="../public/client/css/bootstrap.min.css">
+  <link rel="stylesheet" href="../public/client/css/all.min.css">
+  <link rel="stylesheet" href="../public/client/css/hover-min.css">
+  <link rel="stylesheet" href="../public/client/css/flag-icons.min.css">
+  <link rel="stylesheet" href="../public/client/css/style.css">
+  <link rel="stylesheet" href="../public/client/slick/slick.css">
+  <!--link(rel='stylesheet' href='client/fonts/fontstyle.css')-->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Muli:300,400,600,700&amp;subset=latin,latin-ext" type="text/css" media="all">
+</head>
+
+<body>
+  <script src="../public/client/js/jquery.min.js"></script>
+  <script src="../public/client/js/all.min.js"></script>
+  <script src="../public/client/js/bootstrap.bundle.min.js"></script>
+  <script src="../public/client/js/moment.min.js"></script>
+  <script src="../public/client/slick/slick.js"></script>
+  <script src="../public/client/js/main.js"></script>
+  <script>
+    $(document).ready(function() {
+        if ($(window).width() > 769) {
+            $('.menu .dropdown > a').click(function() {
+                location.href = this.href;
+            });
+        }
+    });
+</script>
+  <header>
+    <nav class="navbar navbar-expand-lg menu">
+      <div class="container"><a class="navbar-brand" href="#"><img class="img-logo" src="../public/client/images/logo.png"></a>
+        <button class="navbar-toggler border-0" type="button" data-bs-toggle="offcanvas" data-bs-target="#menu" aria-controls="offcanvasNavbar" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+        <div class="search-home-page">
+          <button type="button" class="">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+              <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
+            </svg>
+          </button>
+          <input type="text" placeholder="Tìm kiếm sản phẩm">
+        </div>
+        <div class="offcanvas offcanvas-start" id="menu" tabindex="-1" aria-labelledby="offcanvasNavbarLabel">
+          <div class="offcanvas-header">
+            <div class="offcanvas-title"><img class="img-logo" src="../public/client/images/logo.png"></div>
+            <button class="btn-close" type="button" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+          </div>
+
+          <div class="offcanvas-body">
+            <ul class="navbar-nav justify-content-center flex-grow-1 pe-3 text-uppercase">
+              <li class="nav-item mx-lg-4"><a class="nav-link" href="index.html">Trang Chủ</a></li>
+              <li class="nav-item mx-lg-4"><a class="nav-link" href="product.html">Sản Phẩm</a></li>
+              <li class="nav-item mx-lg-4"><a class="nav-link" href="system.html">Tin tức</a></li>
+              <li class="nav-item mx-lg-4"><a class="nav-link" href="overview.html">Giới thiệu</a></li>
+              <li class="nav-item mx-lg-4"><a class="nav-link" href="contact.html">Liên Hệ</a></li>
+            </ul>
+            <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+              <button class="btn btn-primary me-md-2" type="button">Đăng KÍ</button>
+              <button class="btn btn-primary" type="button">ĐĂng Nhập</button>
             </div>
           </div>
         </div>
-      </nav>
-    </header>
+      </div>
+    </nav>
+  </header>
